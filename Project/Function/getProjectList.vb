70b83b889d99e3f97d2281a5d8d1815013481eb6
wee <weerazero@gmail.com>
2019-02-14 18:07:25 +0700

    #admin

---
+++ Project/Function/getProjectList.vb
@@ -0,0 +1,16 @@
+﻿Public Class getProjectList
+
+
+    Public Function getdata() As ArrayList
+        Dim db As New DataClassesDataContext
+
+
+        Dim query = From ac In db.accounts
+                    Join tm In db.timesheets On ac.id Equals tm.employee_id
+                    Select tm
+        MessageBox.Show(query.Count)
+
+        Return query
+
+    End Function
+End Class
